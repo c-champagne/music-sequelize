a7f890109958428ddad2fcdf83f243d9bd0bbdb0
c-champagne <cassielchampagne@gmail.com>
2020-05-07 20:31:14 -0400

    Completed exercise

---
+++ sequelServer.js
@@ -0,0 +1,99 @@
+
+//Setting up Sequelize and info to connect to db.
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('music_db', 'postgres', '7901', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+
+//Setting up prompt-promise, array to hold inputs
+const prompt = require('prompt-promise');
+let res= [];
+
+//Testing connection - connection worked!
+/* sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection established');
+    })
+    .catch(err => {
+        console.error('Unable to connect', err);
+    }); */
+
+    //Setting up album "model" for the album table using Sequelize.
+    const Model = Sequelize.Model;
+    class album extends Model {}
+
+    album.init({
+        name: {
+            type: Sequelize.STRING,
+            allowNull: true     //For BP, these should probably be false, but did not set when original table was created 
+        },
+        year: {
+            type: Sequelize.INTEGER,
+            allowNull: true
+        },
+        artist_id: {
+            type: Sequelize.INTEGER,
+            allowNull: true
+        },
+    }, {sequelize, modelName: 'album', tableName: 'album', timestamps: false, freezeTableName: true});  //remove timestamps and freeze name
+
+    //Testing Sequalize .create method with hard coded data - worked!
+    /* album.create({name:"Good News", year:"2004", artist_id: 1}).then(album => {
+        console.log("Created album:", album.name)
+    }) */
+
+    //Used .findAll to test db connection again - worked!
+   /*  album.findAll().then((row) => {
+        row.forEach((name) => console.log(name));
+    }) */
+
+
+    //Prompt-chain from previous pg-promise exercise, using Sequelize now.
+    prompt('Album name? ')
+    .then(function albumName(val) {
+        res.push(val);
+        return prompt('Album year? ')
+            .then(function albumYear(val) {
+                res.push(val);
+                return prompt('Artist ID? ')
+                    .then(function artistID(val) {
+                        res.push(val);
+                        console.log(res);
+                        prompt.done();
+                        //Insert goes here
+                        album.create({name: res[0], year: res[1], artist_id: res[2]}).then(album => {
+                            console.log("Created album:", album.name);
+                            //Started working on this piece, not finished/working
+                            /* return prompt('Create another album?')
+                                .then(function addAlbum(response){
+                                    if (response == "yes" || "Yes") {
+                                        res = [];
+                                        prompt('Album name? ')
+                                        .then(albumName(val));
+                                    }
+                                    else {
+                                        prompt.finish();
+                                    }
+                                }) */
+                        })
+                            .catch((e) => {console.error(e)}); 
+                            
+                    })
+                .catch(function rejected(err) {
+                    console.log('error: ', err.stack);
+                    prompt.finish();
+                    
+                })
+                
+            })
+
+    })
+    
+ /*    prompt('Create another album?' )
+    if (response == 'Yes'|| 'yes') {
+        albumPrompt();
+    }
+    else (prompt.finish()); */
---
+++ server.js
@@ -0,0 +1,113 @@
+const config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'music_db',
+    user: 'postgres',
+    password: 7901
+}
+
+const pgp = require('pg-promise')();
+const db = pgp(config);
+
+const co = require('co');
+const prompt = require('prompt-promise');
+let res= [];
+let newArtist= [];
+
+//initial prompt
+/* prompt('Do you want to create an artist, album, song, or songwriter? ')
+    .then(function startCreate (option) {
+        if (option == 'artist' || 'Artist') {
+            
+        }
+    }) */
+
+
+//Create Artist
+ prompt('Artist name? ')
+    .then(function artistName (val) {
+       newArtist.push(val); 
+       console.log(newArtist);
+       prompt.done();
+       //Insert goes here
+       let query = `INSERT INTO artist VALUES (default, $1)`;
+       db.result(query, newArtist)
+           .then((result) => {
+                let findArtist = `SELECT * FROM artist WHERE name=$1`;
+                db.one(findArtist, newArtist)
+                    .then((row) => {
+                        console.log('Created artist with an id of ' + row.id + '.');     
+                        pgp.end();          
+                    })
+           .catch((e) => {console.error(e)});
+            })
+            .catch(function rejected(err) {
+                console.log('error: ', err.stack);
+                prompt.finish();
+   }) 
+    
+// Create Album
+  prompt('Album name? ')
+    .then(function albumName(val) {
+        res.push(val);
+        return prompt('Album year? ')
+            .then(function albumYear(val) {
+                res.push(val);
+                return prompt('Artist ID? ')
+                    .then(function artistID(val) {
+                        res.push(val);
+                        console.log(res);
+                        prompt.done();
+                        //Insert goes here
+                        // let query = `INSERT INTO album VALUES (default, '${res[0], res[1], res[2]}'`;
+                        let query = `INSERT INTO album VALUES (default, $1, $2, $3)`;
+                        // let query = `INSERT INTO album VALUES (default, Test, 2, 3)`
+                        db.result(query, res)
+                            .then(function (result) {
+                                console.log(result);
+                                pgp.end();
+                            })
+                            .catch((e) => {console.error(e)}); 
+                    })
+                .catch(function rejected(err) {
+                    console.log('error: ', err.stack);
+                    prompt.finish();
+                })
+            })
+
+    })
+ 
+
+
+//Create Track
+let newTrack = [];
+prompt('Track name? ')
+    .then(function trackName(val) {
+        newTrack.push(val);
+        return prompt('Track duration? ')
+            .then(function trackDur(val) {
+                newTrack.push(val);
+                return prompt('Artist ID? ')
+                    .then(function artistID(val) {
+                        newTrack.push(val);
+                        console.log(newTrack);
+                        prompt.done();
+                        //Insert goes here
+                        // let query = `INSERT INTO album VALUES (default, '${res[0], res[1], res[2]}'`;
+                        let query = `INSERT INTO song VALUES (default, $1, $2, $3)`;
+                        // let query = `INSERT INTO album VALUES (default, Test, 2, 3)`
+                        db.result(query, newTrack)
+                            .then(function (result) {
+                                console.log(result);
+                                pgp.end();
+                            })
+                            .catch((e) => {console.error(e)}); 
+                    })
+                .catch(function rejected(err) {
+                    console.log('error: ', err.stack);
+                    prompt.finish();
+                })
+            })
+
+    })
+})
